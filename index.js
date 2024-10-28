4ee79e449f458c3f9457bdfa40670828af740537
TypM <tapdecotap2512@gmail.com>
2024-10-28 20:59:02 +0700

    first commit

--- index.js
+++ index.js
@@ -4,15 +4,23 @@ const cors = require('cors')
 const dataRoutes = require('./routes/dataRoutes')
 const functionRoutes = require('./routes/functionRoutes')
 
+const PORT = process.env.PORT || 5000;
+
+
 const app = express();
 app.use(cors());
 app.use(bodyparser.json());
 app.use('/api',dataRoutes);
 app.use('/api',functionRoutes);
 
-const PORT = process.env.PORT || 5000;
-
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 
 app.listen(PORT, (req, res) => {
-  console.log('Suscess to connect server!'); //ให้serverเห็น
+  console.log('Suscess to connect server!');
 })

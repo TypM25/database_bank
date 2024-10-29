a01ad0e67a821c8d372f1e03393a1840e1db69c1
TypM <tapdecotap2512@gmail.com>
2024-10-29 10:35:10 +0700

    d

--- db.js
+++ db.js
@@ -1,15 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_DATABASE,
-  password: process.env.DATABASE_PASSWORD,
-  port: 5432,
+  connectionString: "postgresql://pin:XJQWxmZP7d7RZUZlTKjGwCmiswr7HdPo@dpg-csfrkltsvqrc739qoru0-a.singapore-postgres.render.com/dbbank_dbpb",
   ssl: {
     rejectUnauthorized: false,
   },
 });
-
 
 module.exports = pool;

790db6b4d2ff016159c591b23c89c1857524517f
TypM <tapdecotap2512@gmail.com>
2025-03-31 21:45:06 +0700

    d

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: "postgresql://pin:XJQWxmZP7d7RZUZlTKjGwCmiswr7HdPo@dpg-csfrkltsvqrc739qoru0-a.singapore-postgres.render.com/dbbank_dbpb",
   ssl: {
-    rejectUnauthorized: true, // หรือสามารถใช้ `sslmode` เป็น 'require' แทน
+    rejectUnauthorized: false, // หรือสามารถใช้ `sslmode` เป็น 'require' แทน
   },
 });
 

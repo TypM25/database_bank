c30b00d2b03154da9198afaad98c69456b65cb4b
TypM <tapdecotap2512@gmail.com>
2025-03-31 21:13:26 +0700

    first commit

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: "postgresql://pin:XJQWxmZP7d7RZUZlTKjGwCmiswr7HdPo@dpg-csfrkltsvqrc739qoru0-a.singapore-postgres.render.com/dbbank_dbpb",
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: true, // หรือสามารถใช้ `sslmode` เป็น 'require' แทน
   },
 });
 
